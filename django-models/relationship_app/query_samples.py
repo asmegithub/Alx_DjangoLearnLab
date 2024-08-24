abfaa3b76531576b14d904a0aef59a93e78c5c39
Asmare Zelalem <asmarestu12@gmail.com>
2024-08-24 14:49:21 +0300

    minor change

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,4 +1,4 @@
-from relationship_app.models import Book, Author, Library
+from relationship_app.models import Book, Author, Library, Librarian
 
 # Query all books by a specific author
 author_name = 'John Doe'
@@ -9,11 +9,11 @@ print(books_by_author)
 # List all books in a library
 library_name = 'Main Library'
 library = Library.objects.get(name=library_name)
-books_in_library = library.books.all()  # Add this line
+books_in_library = library.books.all()
 print(books_in_library)
 
 # Retrieve the librarian for a library
 library_name = 'Main Library'
 library = Library.objects.get(name=library_name)
-librarian = library.librarian
+librarian = Librarian.objects.get(library=library)
 print(librarian)
