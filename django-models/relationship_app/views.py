76bd2b2cb96120afb285156ed9582738282d4778
Asmare Zelalem <asmarestu12@gmail.com>
2024-08-25 12:00:28 +0300

    Refactor code to remove unused import statement and update file paths

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .models import Book
 from .models import Library
-from django.contrib.auth import login
+# Remove the unused import statement
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 
@@ -10,17 +10,17 @@ from django.contrib.auth.views import LoginView
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
-    return render(request, 'relationship_applist_books.html', context)
+    return render(request, './templates/list_books.html', context)
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = './templates/'
+    template_name = './templates/library_detail.html'
     context_object_name = 'library'
 
 
 class LoginView(LoginView):
-    template_name = './templates/'
+    template_name = './templates/users/login.html'
 
 
 def register(request):
