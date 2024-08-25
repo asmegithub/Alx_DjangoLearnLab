7401b9211cb70a3f3ef3b840aefac3d0db19258c
Asmare Zelalem <asmarestu12@gmail.com>
2024-08-25 09:55:10 +0300

    Refactor code to include user registration functionality

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def list_books(request):
