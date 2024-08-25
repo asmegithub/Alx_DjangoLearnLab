40fc5a0624e7d576d10f592400f1f1849596c386
Asmare Zelalem <asmarestu12@gmail.com>
2024-08-25 10:18:40 +0300

    chore: Refactor code to include user registration functionality

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import LibraryDetailView
-from .views import list_books
+from . import views
+from django.urls import path
+from .views import LibraryDetailView, list_books
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/', LibraryDetailView.as_view(), name='library_detail'),
+    # Add your new paths here
+    path('register/', views.register, name='register'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .models import Book, Library
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 
 
 def list_books(request):
@@ -15,3 +16,19 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+
+
+class LoginView(LoginView):
+    template_name = 'login.html'
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
