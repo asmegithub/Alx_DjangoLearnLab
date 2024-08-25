41e2552062781c28dfb3ec6e0f6ca897dc8be13d
Asmare Zelalem <asmarestu12@gmail.com>
2024-08-25 09:52:32 +0300

    Refactor code to update file paths and remove unused templates

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,10 +6,10 @@ from .models import Book, Library
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
-    return render(request, 'relationship_app/book_list.html', context)
+    return render(request, 'book_list.html', context)
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'library_detail.html'
     context_object_name = 'library'
