293378a144df8833b5e9c034436ad3f45d1dc885
Asmare Zelalem <asmarestu12@gmail.com>
2024-08-25 21:48:00 +0300

    minor change

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,31 +1,19 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from relationship_app.models import Book, Author, Library, Librarian
 
 # Query all books by a specific author
-
-
-def get_books_by_author(author):
-    try:
-        author = Author.objects.get(name=author)
-        return author.books.all()
-    except Author.DoesNotExist:
-        return None
+author_name = 'John Doe'
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(books_by_author)
 
 # List all books in a library
-
-
-def get_books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return None
+library_name = 'Main Library'
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(books_in_library)
 
 # Retrieve the librarian for a library
-
-
-def get_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.librarian
-    except Library.DoesNotExist:
-        return None
+library_name = 'Main Library'
+library = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=library)
+print(librarian)
