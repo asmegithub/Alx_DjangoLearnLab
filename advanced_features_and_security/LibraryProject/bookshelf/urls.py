d1404165e092f1b866f743d395ac26d794f08ca3
Asmare Zelalem <asmarestu12@gmail.com>
2024-09-01 18:16:26 +0300

    minnor change

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import list_books, view_book, create_book, edit_book, delete_book
+from .views import book_list, view_book, create_book, edit_book, delete_book
 
-from . import views
 urlpatterns = [
-    path('', list_books, name='list_books'),
+    path('', book_list, name='list_books'),
     path('create_book/', create_book, name='create_book'),
     path('view_book/<int:book_id>', view_book, name='view_book'),
     path('edit_book/<int:book_id>', edit_book, name='edit_book'),
@@ -12,5 +11,4 @@ urlpatterns = [
     # path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='bookshelf/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -11,7 +11,7 @@ def index(request):
     return HttpResponse("Hey this book shelf page!")
 
 
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/list_books.html', {'books': books})
 
