a3bba6e3e92eee662cf635472c5f62595511edaf
Asmare Zelalem <asmarestu12@gmail.com>
2024-09-07 11:08:34 +0300

    minor change

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView  # this has only GET method
+from rest_framework import generics
+# generics.ListAPIview=>  this has only GET method
+
 # this has both GET and POST method
 from rest_framework.generics import ListCreateAPIView
+
 
 from .serializers import BookSerializer
 from .models import Book
 
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
