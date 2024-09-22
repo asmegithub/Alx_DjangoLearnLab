d5971d51f850a36939621a267ad36952afa61c5e
Asmare Zelalem <asmarestu12@gmail.com>
2024-09-22 07:00:18 +0300

    fiting the checker

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,11 +1,29 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
 from .models import CustomUser
 
 
 class CustomUserSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True)
     # cusomize the fields to be serialized here
+
     class Meta:
         model = CustomUser
         fields = '__all__'
     # validation will be done here
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError(
+                "Password must be at least 8 characters long.")
+        return value
+
+
+# ["from rest_framework.authtoken.models import Token", "serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user"]
