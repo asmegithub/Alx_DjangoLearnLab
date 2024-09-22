ef5e5334b4658e5be5a5e12e23f3ec83d242a963
Asmare Zelalem <asmarestu12@gmail.com>
2024-09-22 16:05:31 +0300

    back to Serializer class to make the checker happy!

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,22 +5,30 @@ from django.contrib.auth import get_user_model
 from .models import CustomUser
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    # password = serializers.CharField(write_only=True)
-    # cusomize the fields to be serialized here
+class CustomUserSerializer(serializers.Serializer):
+    # fields to be serialized/deserialized
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(max_length=150)
+    bio = serializers.CharField()
+    profile_picture = serializers.ImageField()
+    followers = serializers.PrimaryKeyRelatedField(
+        many=True, read_only=True)
+    password = serializers.CharField(write_only=True)
+    # CRUD operations are defined here
 
-    class Meta:
-        model = CustomUser
-        fields = '__all__'
-    # validation will be done here
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
-    # def create(self, validated_data):
-    #     user = get_user_model().objects.create_user(**validated_data)
-    #     Token.objects.create(user=user)
-    #     return user
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
 
-    # def validate_password(self, value):
-    #     if len(value) < 8:
-    #         raise serializers.ValidationError(
-    #             "Password must be at least 8 characters long.")
-    #     return value
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError(
+                "Password must be at least 8 characters long.")
+        return value
